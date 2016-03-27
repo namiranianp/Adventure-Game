acee5a27f1e2c0ed79ec7a65687166e95b2573cd
Pedram Namiranian <Pedram Namiranian>
2016-03-26 21:15:42 -0700

    Making Packages

---
+++ src/View/Game_View.java
@@ -0,0 +1,13 @@
+package View;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Game_View {
+	private class mainViewPanel extends JPanel {
+		public void paintComponent(Graphics g) {
+
+		}
+	}
+}
