6f32ae2e42a236dbcab4556cd4b6501a6e3340ef
Vybhav B <vybs2499@gmail.com>
2016-03-26 23:34:55 -0700

    Changing controller
    Now contains necessary methods for view and model

--- src/Controller/Game_Controller.java
+++ src/Controller/Game_Controller.java
@@ -1,5 +1,22 @@
 package Controller;
 
+import Model.Game_Model;
+
 public class Game_Controller {
+
+    Game_Controller controller = new Game_Controller();
+    Game_Model model = new Game_Model();
+
+    public int getBoardSize() {
+        return model.getBoardSize();
+    }
+
+    public static void setBoardSize(int boardSize) {
+        Game_Controller.boardSize = boardSize;
+    }
+
+    public static int getBoardLength() {
+        return boardLength;
+    }
 
 }
--- src/View/Game_View.java
+++ src/View/Game_View.java
@@ -1,8 +1,9 @@
 package View;
 
-import java.awt.Graphics;
+import Controller.Game_Controller;
 
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 
 public class Game_View {
 	
@@ -13,7 +14,7 @@ public class Game_View {
 	
 	private class mainViewPanel extends JPanel {
 		public void paintComponent(Graphics g) {
-			for(int i=0; i<Game_Controller.getBoardSize(); i++){
+			for(int i = 0; i< Game_Controller.getBoardSize(); i++){
 				for(int j=0; j<Game_Controller.getBoardLength(); j++){
 					
 				}
