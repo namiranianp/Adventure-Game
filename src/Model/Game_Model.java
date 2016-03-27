3841a8c156db919e8eb43849e26fdcbeb7af0eb5
richardbao97 <richard.bao97@gmail.com>
2016-03-27 13:04:31 -0700

    Update Game_Model.java

--- src/Model/Game_Model.java
+++ src/Model/Game_Model.java
@@ -2,29 +2,27 @@ package Model;
 
 import java.util.ArrayList;
 import java.awt.image.BufferedImage;
-import java.io.*;
 
-//Custom experimentalcode, this is all pseudo code
-public class Game_Model(){
-	//File
-	String filename = "null";
-	
+//Custom experimental code, this is all pseudo code
+public class Game_Model{
 	//Player location
 	private int posx;
 	private int posy;
-	
-	
 	//Map size
 	private int boardSize;
 	//array of map (holds map data)
 	private BufferedImage[][] map;
 	
-	//Inventory --> takes in ints that corrospond to items
+	//Inventory --> takes in integers that correspond to items
 	private ArrayList<Integer> inv = new ArrayList<Integer>();
 	
 	//Maybe take in current map size 
 	//(unless we add collision with certain objects)
-	public Adventure_Game_Model(){
+	public Game_Model(){
+		//Not sure what to put in here, 
+		// maybe take in map size? and
+		//starting position
+		
 		//Test purposes
 		posx = 1;
 		posy = 1;
@@ -37,19 +35,6 @@ public class Game_Model(){
 	//else it will create a document named save.txt
 	public void readFile(){
 		//if there is no save data on document, create one.
-		//Test --> txt file should read for map in folder
-		filename = "mapdata.txt"; //or similar name
-		try {
-            		FileReader fileReader = new FileReader(fileName);
-        		BufferedReader bufferedReader = new BufferedReader(fileReader);
-            
-            		//Test
-        		 while((line = bufferedReader.readLine()) != null) {
-        			   System.out.println(line);
-        		 }   
-        		  // Always close files.
-        		 bufferedReader.close();         
-        	}
 	}
 	
 	//Saves file for character (should create a txtfile containing data)
@@ -59,14 +44,8 @@ public class Game_Model(){
 	
 	//creates an array of the map and holds all the data of the blocks
 	private void initializeMap(int size){
-		map = new String[size][size];
-		/* this should use file data to setup array data
-		for(int row = 0; row < map.length; row++){
-			for(int col = 0; col < map[0].length; col++){
-				map[row][col] = "null";
-			}
-		}
-		*/
+		map = new BufferedImage[size][size];
+		// this should use file data to setup array data
 	}
 	
 	//Sets the data on what the map looks like
@@ -74,7 +53,7 @@ public class Game_Model(){
 		map[y][x] = pic;
 	}
 	
-	//Returns number corrolating to picture
+	//Returns number correlating to picture
 	public BufferedImage getImage(int x, int y){
 		return map[y][x];
 	}
@@ -100,28 +79,28 @@ public class Game_Model(){
 		}
 	}
 	
-	public void getPosX(){
+	public int getPosX(){
 		return posx;
 	}
-	public void getPosY(){
+	public int getPosY(){
 		return posy;
 	}
 	
-	public void getBoardSize(){
+	public int getBoardSize(){
 		return boardSize;
 	}
 	
 	private boolean inBounds(int x, int y){
-		return(x < boardSize && x > 0 && y < boardSize && y > 0){
+		return(x < boardSize && x > 0 && y < boardSize && y > 0);
 	}
 	
 	public void giveInv(int itemCode){
-		inv.add(item);
+		inv.add(itemCode);
 	}
 	
 	public void removeItem(int itemCode){
 		for(int i = 0; i < inv.size(); i++){
-			if(inv.get(i) == itemcode){
+			if(inv.get(i) == itemCode){
 				inv.remove(i);
 				break;
 			}
