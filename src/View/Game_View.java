4403daeae80cf5dbd2ad7f6225b0369c68aa40ab
Pedram Namiranian <Pedram Namiranian>
2016-03-26 23:21:38 -0700

    Basic view

--- src/View/Game_View.java
+++ src/View/Game_View.java
@@ -5,9 +5,19 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class Game_View {
+	
+	public Game_View(){
+		
+	}
+	
+	
 	private class mainViewPanel extends JPanel {
 		public void paintComponent(Graphics g) {
-
+			for(int i=0; i<Game_Controller.getBoardSize(); i++){
+				for(int j=0; j<Game_Controller.getBoardLength(); j++){
+					
+				}
+			}
 		}
 	}
 }
