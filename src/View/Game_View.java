d06cab3a80986133ee648e7f9c71f74b4a22df0a
Pedram Namiranian <Pedram Namiranian>
2016-03-27 22:24:17 -0700

    adding better view things

--- src/View/Game_View.java
+++ src/View/Game_View.java
@@ -1,20 +1,7 @@
 package View;
 
-<<<<<<< HEAD
-import Controller.Game_Controller;
-
-import javax.swing.*;
-
-public class Game_View {
-
-	public Game_Controller control;
-	public GamePanel gameBoard;
-
-
-=======
-import java.awt.event.KeyAdapter;
-
 import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
 import Controller.Game_Controller;
 
@@ -22,24 +9,23 @@ public class Game_View {
 	Game_Controller control;
 	JFrame window;
 	GamePanel gameBoard;
-	
->>>>>>> origin/master
+
 	public Game_View(Game_Controller c) {
 		control = c;
-		
+
 		window = new JFrame();
 		window.setBounds(100, 20, 655, 700);
-		
+
 		gameBoard = new GamePanel(control);
 
 		// LAST
 		window.getContentPane().add(gameBoard);
 		window.setVisible(true);
-        window.setResizable(false);
-        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
-	
-	public void addKeyAdapter(CustomKeyListener listen){
+
+	public void addKeyAdapter(CustomKeyListener listen) {
 		window.addKeyListener(listen);
 	}
 }
