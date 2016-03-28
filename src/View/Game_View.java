55e1b84cc1a65d7e00739081e70719fa787528bc
Pedram Namiranian <Pedram Namiranian>
2016-03-27 21:31:08 -0700

    adding ways to add listeners

--- src/View/Game_View.java
+++ src/View/Game_View.java
@@ -1,27 +1,30 @@
 package View;
 
-import java.awt.Dimension;
+import java.awt.event.KeyAdapter;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import Controller.Game_Controller;
 
-import javax.swing.*;
-import java.awt.*;
+import Controller.Game_Controller;
 
 public class Game_View {
 	Game_Controller control;
-
+	JFrame window;
+	GamePanel gameBoard;
+	
 	public Game_View(Game_Controller c) {
 		control = c;
 		
-		JFrame window = new JFrame();
+		window = new JFrame();
 		window.setBounds(100, 20, 655, 700);
 		
-		GamePanel gameBoard = new GamePanel(control);
+		gameBoard = new GamePanel(control);
 
 		// LAST
 		window.getContentPane().add(gameBoard);
 		window.setVisible(true);
+	}
+	
+	public void addKeyAdapter(CustomKeyListener listen){
+		window.addKeyListener(listen);
 	}
 }
