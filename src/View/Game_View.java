0ead72c7a5851bfa79467efb33a28383b725b74f
Pedram Namiranian <Pedram Namiranian>
2016-03-27 22:29:17 -0700

    merging

--- src/View/Game_View.java
+++ src/View/Game_View.java
@@ -1,25 +1,16 @@
 package View;
 
-<<<<<<< HEAD
-import Controller.Game_Controller;
-
-import javax.swing.*;
 import java.awt.event.KeyAdapter;
-=======
+
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
 import Controller.Game_Controller;
->>>>>>> Pedram_Changes
 
 public class Game_View {
 	Game_Controller control;
 	JFrame window;
-<<<<<<< HEAD
 	public GamePanel gameBoard;
-=======
-	GamePanel gameBoard;
->>>>>>> Pedram_Changes
 
 	public Game_View(Game_Controller c) {
 		control = c;
