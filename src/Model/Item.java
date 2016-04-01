36736108f32fd79c6816266006953825175e7994
Pedram Namiranian <Pedram Namiranian>
2016-03-31 20:57:26 -0700

    why

--- src/Model/Item.java
+++ src/Model/Item.java
@@ -1,26 +1,23 @@
 package Model;
+
 public class Item {
 	String name;
 	int buyValue;
 	int sellValue;
-<<<<<<< HEAD
+
 	boolean equiped;
 
-
 	public Item(String itemName, int buy, int sell, boolean isEquiped) {
-=======
-	
-	public Item(String itemName, int buy, int sell) {
->>>>>>> Pedram_Changes
 		name = itemName;
 		buyValue = buy;
 		sellValue = sell;
 	}
-	
-	public int getBuyValue(){
+
+	public int getBuyValue() {
 		return buyValue;
 	}
-	public int getSellValue(){
+
+	public int getSellValue() {
 		return sellValue;
 	}
 }
