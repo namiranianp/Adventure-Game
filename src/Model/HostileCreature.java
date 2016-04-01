e20b061720bc11243d2242828ebb710b14db46ed
richardbao97 <richard.bao97@gmail.com>
2016-03-31 21:41:39 -0700

    Create HostileCreature.java

---
+++ src/Model/HostileCreature.java
@@ -0,0 +1,23 @@
+package Model;
+public class HostileCreature extends Creature{
+  public HostileCreature(){
+    //Pass in hp, atk, how fast it moves,if it drops items
+  }
+  
+  /*
+  //Get player pos and slowly go towards there (if near a radius of 5 or something)
+  if (creature.getXPos() < player.getYPos()){
+  //Might want to add a counter or something to make creature slowly go towards player
+    creature XPos += 1;
+}else if (creature.getXPos() > player.getXPos()){
+    creature Xpos -= 1;
+}
+if (creature.getYPos() < player.getYPos()){
+    creature Ypos += 1;
+}else if (creature.getYPos() > player.getYPos()){
+    creature Ypos -= 1;
+}
+*/
+
+
+}
