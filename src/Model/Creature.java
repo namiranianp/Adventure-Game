7b7014f778d1cb6938326906bdd28120c98bb0bc
Vybhav B <vybs2499@gmail.com>
2016-04-01 16:00:58 -0700

    idk

--- src/Model/Creature.java
+++ src/Model/Creature.java
@@ -1,41 +1,44 @@
 package Model;
 
-int hp = 3, atk = 1;
-String name;
-boolean canDrop, canTalk, respawnable;
-//Add different movement types
 
 public class Creature {
-  public Creature{
-    name = "generic npc name";
-    canDrop = false;
-    canTalk = false;
-    respawnable = false;
-  }
 
- public int getHp(){
-   return hp;
- }
-public void setHp(int num){
-   hp = num;
- }
- public void takeDmg(int num){
-   if(num >= hp){
-     hp = 0;
-   }
- }
-public int getAtk(){
-  return atk;
-}
-public void setAtk(int num){
-  atk = num;
-}
-public String getName(){
-  return name;
-}
-public void setName(String txt){
-  name = txt;
-}
+
+    int hp = 3, atk = 1;
+    String name;
+    boolean canDrop, canTalk, respawnable;
+
+    //Add different movement types
+    public Creature(){
+        name = "generic npc name";
+        canDrop = false;
+        canTalk = false;
+        respawnable = false;
+    }
+
+    public int getHp(){
+        return hp;
+    }
+    public void setHp(int num){
+        hp = num;
+    }
+    public void takeDmg(int num){
+        if(num >= hp){
+            hp = 0;
+        }
+    }
+    public int getAtk(){
+        return atk;
+    }
+    public void setAtk(int num){
+        atk = num;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String txt){
+        name = txt;
+    }
 //NPC
 /*
 //talkable enemys maybe?
