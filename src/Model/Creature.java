d460060e71e688cc7a54ebbcd84e78edf83a81cf
Vybhav B <vybs2499@gmail.com>
2016-04-02 20:40:37 -0700

    Changing monitor

--- src/Model/Creature.java
+++ src/Model/Creature.java
@@ -1,45 +1,50 @@
 package Model;
 
-<<<<<<< HEAD
-
 public class Creature {
-
 
     int hp = 3, atk = 1;
     String name;
     boolean canDrop, canTalk, respawnable;
 
     //Add different movement types
-    public Creature(){
+    public Creature() {
         name = "generic npc name";
         canDrop = false;
         canTalk = false;
         respawnable = false;
     }
 
-    public int getHp(){
+    public int getHp() {
         return hp;
     }
-    public void setHp(int num){
+
+    public void setHp(int num) {
         hp = num;
     }
-    public void takeDmg(int num){
-        if(num >= hp){
+
+    public void takeDmg(int num) {
+        if (num >= hp) {
             hp = 0;
         }
     }
-    public int getAtk(){
+
+    public int getAtk() {
         return atk;
     }
-    public void setAtk(int num){
+
+    public void setAtk(int num) {
         atk = num;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public void setName(String txt){
+
+    public void setName(String txt) {
         name = txt;
     }
+
+}
 //NPC
 /*
 //talkable enemys maybe?
@@ -50,16 +55,3 @@ public boolean talkable(){
   return false;
 }
 */
-=======
-public class Creature {
-	String name;
-
-	public Creature(String imageName) {
-		name = imageName;
-	}
-	
-	public String getName(){
-		return name;
-	}
->>>>>>> f7acd6a9aa42a97d6924e5b3752fa5f0e289d5ec
-}
