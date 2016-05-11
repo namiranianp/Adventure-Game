797ca1bfb4de213ed54233931deab6fc5e43d2bd
Vybhav B <vybs2499@gmail.com>
2016-05-11 08:08:16 -0700

    changes

--- src/Model/Game_Model.java
+++ src/Model/Game_Model.java
@@ -52,7 +52,7 @@ public class Game_Model {
 		Timer t = new Timer(1000, d -> {
             System.out.println(test.getxPos() + " , " + test.getyPos());
 			moveCreature();
-			//damagePlayer();
+			damagePlayer();
 		});
 		t.start();
 	}
@@ -123,6 +123,7 @@ public class Game_Model {
 	}
 
 	public void shoot() {
+		System.out.println("SHOOTING!!!!");
 		if (guy.getDirection() == UP) {
 			for (int i = yPos; i >= 0; i--) {
 				if (gameBoard[xPos][i].hasTerrain())
